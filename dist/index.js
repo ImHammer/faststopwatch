4edb49bc34150765a66f92885164ae5605ff3b9e
ImHammer <hamemrdevofc@gmail.com>
2022-06-16 22:38:58 -0300

    Created tray menu

--- dist/index.js
+++ dist/index.js
@@ -2,21 +2,48 @@
 exports.__esModule = true;
 var electron_1 = require("electron");
 var path = require("path");
-function createAboutWindow() {
-    var aboutWindow = new electron_1.BrowserWindow({
-        width: 400,
-        height: 400,
-        webPreferences: {
-            preload: path.join(__dirname, "aboutPreload.js")
-        }
-    });
-    aboutWindow.setTitle("About");
-    aboutWindow.setMenuBarVisibility(false);
-    aboutWindow.loadFile(path.join(__dirname, "About.html"));
+var aboutWindow;
+function showAboutWindow() {
+    if (aboutWindow != undefined) {
+        aboutWindow.focus();
+    }
+    else {
+        aboutWindow = new electron_1.BrowserWindow({
+            width: 400,
+            height: 400,
+            webPreferences: {
+                preload: path.join(__dirname, "aboutPreload.js")
+            }
+        });
+        aboutWindow.setTitle("About");
+        aboutWindow.setMenuBarVisibility(false);
+        aboutWindow.loadFile(path.join(__dirname, "About.html"));
+        aboutWindow.on("closed", function () { aboutWindow = undefined; });
+    }
+}
+function showCronometerWindow() {
+    console.log("Cronometer");
+}
+function showTimerWindow() {
+    console.log("Timer");
+}
+// Create app Tray
+function applyTray() {
+    var trayMenu = electron_1.Menu.buildFromTemplate([
+        { label: "Cronometer", type: "normal", click: showCronometerWindow },
+        { label: "Timer", type: "normal", click: showTimerWindow },
+        { label: "About", type: "normal", click: showAboutWindow },
+        { label: "Quit", type: "normal", click: function () { return electron_1.app.quit(); } }
+    ]);
+    var tray = new electron_1.Tray(path.join(__dirname, "assets/iconTemplate.png"));
+    tray.setTitle("FastStopwatch");
+    tray.setToolTip("FastStopwatch");
+    tray.setContextMenu(trayMenu);
 }
 // App ready
 function appReady() {
-    createAboutWindow();
+    showAboutWindow();
+    applyTray();
 }
 // App activate
 // function appActivate(): void {
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,57 @@
 
-import { app, BrowserWindow, ipcMain, nativeTheme, shell } from "electron";
+import { app, BrowserWindow, ipcMain, nativeTheme, shell, Tray, nativeImage, Menu } from "electron";
 import * as path from "path";
 
-function createAboutWindow(): void {
-    const aboutWindow: BrowserWindow = new BrowserWindow({
-        width: 400,
-        height: 400,
-        webPreferences: {
-            preload: path.join(__dirname, "aboutPreload.js")
-        }
-    });
+let aboutWindow: BrowserWindow;
 
-    aboutWindow.setTitle("About");
-    aboutWindow.setMenuBarVisibility(false);
+function showAboutWindow(): void {
+    if (aboutWindow != undefined) {
+        aboutWindow.focus();
+    } else {
+        aboutWindow = new BrowserWindow({
+            width: 400,
+            height: 400,
+            webPreferences: {
+                preload: path.join(__dirname, "aboutPreload.js")
+            }
+        });
 
-    aboutWindow.loadFile(path.join(__dirname, "About.html"));
+        aboutWindow.setTitle("About");
+        aboutWindow.setMenuBarVisibility(false);
+
+        aboutWindow.loadFile(path.join(__dirname, "About.html"));
+
+        aboutWindow.on("closed", () => { aboutWindow = undefined; });
+    }
+}
+
+function showCronometerWindow(): void {
+    console.log("Cronometer");
+}
+
+function showTimerWindow(): void {
+    console.log("Timer");
+}
+
+// Create app Tray
+function applyTray(): void {
+    const trayMenu: Menu = Menu.buildFromTemplate([
+        { label: "Cronometer", type: "normal", click: showCronometerWindow },
+        { label: "Timer", type: "normal", click: showTimerWindow },
+        { label: "About", type: "normal", click: showAboutWindow },
+        { label: "Quit", type: "normal", click: () => app.quit() }
+    ]);
+
+    const tray: Tray = new Tray(path.join(__dirname, "assets/iconTemplate.png"));
+    tray.setTitle("FastStopwatch");
+    tray.setToolTip("FastStopwatch");
+    tray.setContextMenu(trayMenu);
 }
 
 // App ready
 function appReady(): void {
-    createAboutWindow();
+    showAboutWindow();
+    applyTray();
 }
 
 // App activate
